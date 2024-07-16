ca11cddd5a0983c3cd9c5612c8be2bf91472fb4f
김예빈 <162901124+dpqlsn@users.noreply.github.com>
2024-07-17 00:14:04 +0900

    ADD :: Supernatural 슬라이드 자스

---
+++ Supernatural.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    const kindWrap = document.querySelector('.kind_wrap');
+    const slider = kindWrap.querySelector('.slider');
+    const slideLis = slider.querySelectorAll('li');
+    const moveButton = kindWrap.querySelector('.arrow');
+  
+    const liWidth = slideLis[0].clientWidth;
+    const sliderWidth = liWidth * slideLis.length;
+    slider.style.width = `${sliderWidth}px`;
+  
+    let currentIdx = 0; 
+    let translate = 0; 
+  
+    moveButton.addEventListener('click', moveSlide);
+  
+    function moveSlide(event) {
+        event.preventDefault();
+        if (event.target.className === 'next') {
+            if (currentIdx < slideLis.length - 1) {
+                translate -= liWidth;
+                slider.style.transform = `translateX(${translate}px)`;
+                currentIdx += 1;
+            }
+        } else if (event.target.className === 'prev') {
+            if (currentIdx > 0) {
+                translate += liWidth;
+                slider.style.transform = `translateX(${translate}px)`;
+                currentIdx -= 1;
+            }
+        }
+    }
+}
