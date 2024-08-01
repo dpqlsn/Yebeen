cea68c4edfa59361e47b3205759cf1ec6d4b9e72
김예빈 <162901124+dpqlsn@users.noreply.github.com>
2024-08-01 17:49:25 +0900

    ADD :: 시간 스크립트 추가

--- howsweet.js
+++ howsweet.js
@@ -3,18 +3,16 @@ window.onload = function() {
     const slider = kindWrap.querySelector('.slider');
     const slideLis = slider.querySelectorAll('li');
     const moveButton = kindWrap.querySelector('.arrow');
-
+  
     const liWidth = slideLis[0].clientWidth;
     const sliderWidth = liWidth * slideLis.length;
     slider.style.width = `${sliderWidth}px`;
-
-    let currentIdx = 0;
-    let translate = 0;
-
-    slider.style.transform = `translateX(${translate}px)`;
-
+  
+    let currentIdx = 0; 
+    let translate = 0; 
+  
     moveButton.addEventListener('click', moveSlide);
-
+  
     function moveSlide(event) {
         event.preventDefault();
         if (event.target.className === 'next') {
@@ -31,4 +29,16 @@ window.onload = function() {
             }
         }
     }
-};
+}
+
+const clock = document.getElementById("clock");
+
+function getClock() {
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    clock.innerText = `${hour}:${minutes}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
